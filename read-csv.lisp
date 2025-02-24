30eb8b785ebfadb727b7bcdc3405c1a7d629a34a
Mark H. David <mhd@yv.org>
2025-02-23 17:54:22 -0800

    optimized a bit

--- read-csv.lisp
+++ read-csv.lisp
@@ -14,18 +14,32 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defun noop (c) (declare (ignore c)))
   (defun addc (c) (push c *record*))
-  (defun addl (c) (declare (ignore c)) (push #\Newline *record*))
-  (flet ((white (c) (or (char= c #\Space) (char= c #\Tab))))
-    (defun next (c) 
-      (declare (ignore c))
-      (let ((eow (or (position-if-not #'white *record*) (length *record*))))
-        (push (coerce (nreverse (nthcdr (max 0 (min eow (1- *white-char-count*))) *record*)) 'string) *records*))
-      (setf *record* nil)))
+  (defun addl (c) (declare (ignore c)) (push #\Linefeed *record*))
+  (defun next (c) 
+    (declare (ignore c))
+    (let* ((eow
+             (loop with length-so-far fixnum = 0
+                   as c character in *record*
+                   while (or (char= c #\Space) (char= c #\Tab))
+                   do (incf length-so-far)
+                   finally (return length-so-far)))
+           (n (max 0 (min eow (1- *white-char-count*))))
+           (string-contents-in-reverse
+             (nthcdr n *record*))
+           (length (length string-contents-in-reverse))
+           (string
+             (make-array length :element-type 'character)))
+      (loop for i from (1- length) downto 0
+            as char in string-contents-in-reverse
+            do (setf (schar string i) char))
+      (push string *records*))
+    (setf *record* nil))
   (defun ship (c) (next c) (setf *records* (nreverse *records*))))
 
 (defconstant done! -1)
 (defconstant start 0) ;; Start
 (defconstant retur 1)  ;; Return (as in CRLF seen)
+;;^ ---*** no longer used; Return handled in read-csv-char; clean up some day
 (defconstant unquo 2)  ;; Unquoted text
 (defconstant myquo 3)  ;; Quoted text
 (defconstant q+ret 4)  ;; Quoted and have seen return
@@ -45,18 +59,21 @@
 	 ((noop ,q+q&w) (ship ,retur) (ship ,done!) (addc ,myquo) (next ,start) (addc ,unquo))  ;; in-quote, seen quote
 	 ((noop ,q+q&w) (ship ,retur) (ship ,done!) (addc ,myquo) (next ,start) (addc ,unquo)))))) ;; in quote, seen quote, now whitespace
 
+(declaim (inline char-class))
 (defun char-class (sep char)
+  (declare (type character sep char))
   (case char (#\Space 0) (#\Return 1) (#\Linefeed 2) (#\" 3) (otherwise (if (char= sep char) 4 5))))
 
+(declaim (inline read-csv-char))
 (defun read-csv-char (stream eof-error-p)
   (let ((char-or-eof (read-char stream eof-error-p :eof)))
     (case char-or-eof
-      (#\return
+      (#\Return
        ;; CR: if followed by LF, read past it. Either way, return LF.
-       (when (eql (peek-char nil stream eof-error-p :eof) #\linefeed)
+       (when (eql (peek-char nil stream eof-error-p :eof) #\Linefeed)
          (read-char stream))
-       #\linefeed)
-      (otherwise char-or-eof))))       
+       #\Linefeed)
+      (otherwise char-or-eof))))
 
 (defun read-csv (stream &optional (sep #\,) (eof-error-p t) eof-value)
   "Read a single line of CSV data from stream. Return the parsed CSV
@@ -69,21 +86,30 @@
   (let ((*records* nil)
         (*record* nil)
         (*white-char-count* 0))
-    (declare (special *record* *records* *white-char-count*))
-    (loop with state = start
+    (declare (special *record* *records* *white-char-count*)
+             (type fixnum *white-char-count*))
+    (loop with state fixnum = start
           for char = (read-csv-char stream (and (null *records*) eof-error-p))
           when (eq char :eof) 
           do (return-from read-csv (values (if *records* (ship :eof) eof-value) t))
           do (incf *white-char-count*)
           do (let ((class (char-class sep char)))
+               (declare (type fixnum class))
                (when (= class myquo) (setf *white-char-count* 0))
                (funcall (aref +csv-table+ state class 0) char)
                (setf state (aref +csv-table+ state class 1)))
-       until (eql state done!))
+          until (= state done!))
     (values *records* (eq :eof (peek-char nil stream nil :eof)))))
  
 (defun parse-csv (stream  &optional (sep #\,))
   "Read CSV data from a stream until end-of-file is encountered."
-  (loop for (line end-p) = (multiple-value-list (read-csv stream sep nil :eof))
-        unless (eq line :eof) collect line
+  ;; (loop for (line end-p) = (multiple-value-list (read-csv stream sep nil :eof))
+  ;;       unless (eq line :eof) collect line
+  ;;         until end-p)
+  (loop with line
+        with end-p
+        do (multiple-value-setq (line end-p)
+             (read-csv stream sep nil :eof))
+        unless (eq line :eof)
+          collect line
         until end-p))
